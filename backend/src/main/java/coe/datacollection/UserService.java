b2c6e9235b58156a40fb0f3fd0b1f7a8b2c963e1
Walle10-0 <68664253+Walle10-0@users.noreply.github.com>
2024-04-04 13:19:43 -0400

    fixes and code format

--- backend/src/main/java/coe/datacollection/UserService.java
+++ backend/src/main/java/coe/datacollection/UserService.java
@@ -9,47 +9,46 @@ import java.util.List;
 
 @Service
 public class UserService {
-    @Autowired
-    private UserRepository UserRepository;
+	@Autowired
+	private UserRepository userRepository;
 	
 	@Autowired
 	private GenericRepository genericRepository;
 
-    // create a new user
-    public UserDTO createUser(UserDTO userDTO) {
-        User user = new User();
-        user.setFirstName(userDTO.getFirstName());
-        user.setLastName(userDTO.getLastName());
-        user = UserRepository.save(user);
-        return convertToDTO(user);
-    }
+	// create a new user
+	public UserDTO createUser(UserDTO userDTO) {
+		User user = new User();
+		user.setFirstName(userDTO.getFirstName());
+		user.setLastName(userDTO.getLastName());
+		user = userRepository.save(user);
+		return convertToDTO(user);
+	}
 
-    // retrieve all users
-    public List<UserDTO> getAllUsers() {
-		return convertToDTO(UserRepository.findAll());
-    }
+	// retrieve all users
+	public List<UserDTO> getAllUsers() {
+		return convertToDTO(userRepository.findAll());
+	}
 
-    public UserDTO getUser(Long userId) {
-        User currentUser = UserRepository.findById(userId).orElse(null);
-        return currentUser != null ? convertToDTO(currentUser) : null;
-    }
+	public UserDTO getUser(Long userId) {
+		User currentUser = userRepository.findById(userId).orElse(null);
+		return currentUser != null ? convertToDTO(currentUser) : null;
+	}
 
-    // update an existing user
-    public UserDTO updateUser(Long id, UserDTO userDTO) {
-        User cUser = UserRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not found with id: " + id)); // Customize exception as
-                                                                                           // needed
+	// update an existing user
+	public UserDTO updateUser(Long id, UserDTO userDTO) {
+		User cUser = userRepository.findById(id)
+			.orElseThrow(() -> new RuntimeException("User not found with id: " + id)); // Customize exception as needed
 		User nUser = convertFromDTO(userDTO);
 		System.out.println("id " + id);
 		System.out.println("uid " + cUser.getUserId());
-        	cUser.setFirstName(nUser.getFirstName() == null ? cUser.getFirstName() : nUser.getFirstName());
+		cUser.setFirstName(nUser.getFirstName() == null ? cUser.getFirstName() : nUser.getFirstName());
 		cUser.setLastName(nUser.getLastName() == null ? cUser.getLastName() : nUser.getLastName());
 		cUser.setDepartment(nUser.getDepartment() == null ? cUser.getDepartment() : nUser.getDepartment());
 		//cUser.setRoleName(nUser.getUserRole() == null ? cUser.getUserRole() : nUser.getUserRole());
 		
-		userDTO.setLoad(user.getLoad() == null ? null : user.getLoad().getLoad());
-		userDTO.setRank(user.getRank() == null ? null : user.getRank().getRank());
-		userDTO.setStatus(user.getStatus() == null ? null : user.getStatus().getStatus());
+		cUser.setLoad(nUser.getLoad() == null ? cUser.getLoad() : nUser.getLoad());
+		cUser.setRank(nUser.getRank() == null ? cUser.getRank() : nUser.getRank());
+		cUser.setStatus(nUser.getStatus() == null ? cUser.getStatus() : nUser.getStatus());
 		
 		cUser.setJournals(nUser.getJournals());
 		cUser.setConferences(nUser.getConferences());
@@ -69,28 +68,28 @@ public class UserService {
 		cUser.setClasses(nUser.getClasses() == null ? cUser.getClasses() : nUser.getClasses());
 		cUser.setServiceActivity(nUser.getServiceActivity() == null ? cUser.getServiceActivity() : nUser.getServiceActivity());
 
-        cUser = UserRepository.save(cUser);
-        return convertToDTO(cUser);
-    }
+		cUser = userRepository.save(cUser);
+		return convertToDTO(cUser);
+	}
 
-    // delete existing user
-    public void deleteUser(Long id) {
-        UserRepository.deleteById(id);
-    }
-
-    // convert User to UserDTO
-    private UserDTO convertToDTO(User user) {
-        UserDTO userDTO = new UserDTO();
+	// delete existing user
+	public void deleteUser(Long id) {
+		userRepository.deleteById(id);
+	}
+	
+	// convert User to UserDTO
+	private UserDTO convertToDTO(User user) {
+		UserDTO userDTO = new UserDTO();
 		
-        userDTO.setId(user.getUserId());
-        userDTO.setFirstName(user.getFirstName());
-        userDTO.setLastName(user.getLastName());
+		userDTO.setId(user.getUserId());
+		userDTO.setFirstName(user.getFirstName());
+		userDTO.setLastName(user.getLastName());
 		userDTO.setDepartment(user.getDepartment().getDepartment());
-        userDTO.setRoleName(user.getUserRole().getRoleName());
+		userDTO.setRoleName(user.getUserRole().getRoleName());
 		
-		userDTO.setLoad(user.getLoad().getLoad());
-		userDTO.setRank(user.getRank().getRank());
-		userDTO.setStatus(user.getStatus().getStatus());
+		userDTO.setLoad(user.getLoad() == null ? null : user.getLoad().getLoad());
+		userDTO.setRank(user.getRank() == null ? null : user.getRank().getRank());
+		userDTO.setStatus(user.getStatus() == null ? null : user.getStatus().getStatus());
 		
 		userDTO.setJournals(user.getJournals());
 		userDTO.setConferences(user.getConferences());
@@ -110,28 +109,28 @@ public class UserService {
 		userDTO.setClasses(user.getClasses());
 		userDTO.setServiceActivity(user.getServiceActivity());
 		
-        return userDTO;
-    }
+		return userDTO;
+	}
 	
 	// bulk convert
 	private List<UserDTO> convertToDTO(List<User> user) {
-        List<UserDTO> DTOList = new ArrayList<UserDTO>();
-        for (User current : user) {
+		List<UserDTO> DTOList = new ArrayList<UserDTO>();
+		for (User current : user) {
 			DTOList.add(convertToDTO(current));
 		}
 		
-        return DTOList;
-    }
+		return DTOList;
+	}
 
-    // convert UserDTO to User
-    public User convertFromDTO(UserDTO dto) {
-        User user = new User();
-        user.setUserId(dto.getId());
-        user.setFirstName(dto.getFirstName());
-        user.setLastName(dto.getLastName());
+	// convert UserDTO to User
+	public User convertFromDTO(UserDTO dto) {
+		User user = new User();
+		user.setUserId(dto.getId());
+		user.setFirstName(dto.getFirstName());
+		user.setLastName(dto.getLastName());
 
 		user.setDepartment(genericRepository.findByString("Department", "deptName", dto.getDepartment()));
-        	user.setUserRole(genericRepository.findByString("UserRole", "roleName", dto.getRoleName()));
+		user.setUserRole(genericRepository.findByString("UserRole", "roleName", dto.getRoleName()));
 		user.setLoad(genericRepository.findByString("CLoad", "load", dto.getLoad()));
 		user.setRank(genericRepository.findByString("URank", "rank", dto.getRank()));
 		user.setStatus(genericRepository.findByString("UStatus", "status", dto.getStatus()));
@@ -219,9 +218,9 @@ public class UserService {
 		}
 		user.setServiceActivity(dto.getServiceActivity());
 
-        // set anything else ...
-        return user;
-    }
+		// set anything else ...
+		return user;
+	}
 
     // export current user to JSON
     public String exportCurrentUserToJSON(Long currentUserId) throws Exception {
@@ -233,7 +232,7 @@ public class UserService {
     }
 	
 	public List<UserDTO> findUsersByDepartmentId(int id) {
-		return convertToDTO(UserRepository.findUsersByDepartmentId(id));
+		return convertToDTO(userRepository.findUsersByDepartmentId(id));
 	}
 	
 	// get All Values for dropdown
